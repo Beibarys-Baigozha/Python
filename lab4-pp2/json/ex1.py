9a21beca1389311e9915c73bff035884d3cd5b6a
Beibarys-Baigozha <90126660+Beibarys-Baigozha@users.noreply.github.com>
2023-02-22 23:17:02 +0600

    Update ex1.py

--- lab4-pp2/json/ex1.py
+++ lab4-pp2/json/ex1.py
@@ -1,24 +1,14 @@
 import json
-
-with open('sample_data.json') as file:
-    json_data = json.load(file)
-
+with open("sample_data.json") as file:
+    data = json.load(file)
+    
 print("Interface Status")
 print("================================================================================")
-print("DN                                                 Description           Speed    MTU")  
+print("DN                                                 Description           Speed    MTU")
 print("-------------------------------------------------- --------------------  ------  ------")
-
-iamdata=json_data["imdata"]
-
-for item in iamdata:
-    nt_item = item["l1PhysIf"]
-    att = nt_item["attributes"]
-    dn = att["dn"]
-    speed = att["speed"]
-    mtu = att["mtu"]
-    output = ""
-    if(len(dn) == 42):
-        output += dn + " " * 30  + speed + "   " + mtu
-    else:
-        output += dn + " " * 31 + speed + "   " + mtu
-    print(output)
+cnt = 0
+for item in data["imdata"]:
+    if cnt == 3:
+        break
+    print(item["l1PhysIf"]["attributes"]["dn"], "                            ",item["l1PhysIf"]["attributes"]["speed"]," ",item["l1PhysIf"]["attributes"]["mtu"]) 
+    cnt += 1   
